6893d845d4cae77cea0e8a3152a88c13035bcb6a
Cdawn99 <140974895+Cdawn99@users.noreply.github.com>
2024-08-01 21:23:01 +0200

    Switch from build script to CMake

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-CFLAGS="-Wall -Werror -Wextra -pedantic -pedantic-errors `pkg-config --cflags raylib`"
-LIBS="`pkg-config --libs raylib` -lm"
-
-mkdir -p ./build/
-gcc -o ./build/main main.c entity.c $CFLAGS $LIBS
